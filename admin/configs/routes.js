b902a8e6fe8b058da9764c067861b23206521d4f
DayraPachas <A15517@lab.aca>
2026-09-25 09:59:35 -0500

    examen

--- admin/configs/routes.js
+++ admin/configs/routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express';
 import * as admins from '../controllers/admin_controllers.js';
 import * as carrers from '../controllers/carrer_controllers.js';
+import * as students from '../controllers/admin_students.js';
 import * as nationApis from '../apis/nations_apis.js';
 import { redirectIfAuthenticated, requireAuth } from '../../configs/middlewares.js'; 
 
@@ -23,4 +24,7 @@ router.delete('/api/v1/nations/:id', nationApis.deleteNation);
 // carrers
 router.get('/admin/carrers', carrers.home);
 
+
+
+router.get('/admin/students', students.home);
 export default router;
